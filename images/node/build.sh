42dcf601bc60b7a255249930dcf64eabf0949c0d
Ralph Wiedemeier <ralph@framefactory.ch>
2023-03-07 11:34:24 +0100

    push node version to 19.7.0

--- images/node/build.sh
+++ images/node/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t framefactory/node:19.6.0 .
-docker tag framefactory/node:19.6.0 framefactory/node:latest
+docker build -t framefactory/node:19.7.0 .
+docker tag framefactory/node:19.7.0 framefactory/node:latest
