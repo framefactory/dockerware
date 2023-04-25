33e9259c5ea4109f0071cd67e591277eb2f41be9
Ralph Wiedemeier <ralph@framefactory.ch>
2023-04-25 14:10:50 +0200

    bump node version

--- images/node/build.sh
+++ images/node/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t framefactory/node:19.7.0 .
-docker tag framefactory/node:19.7.0 framefactory/node:latest
+docker build -t framefactory/node:20.0.0 .
+docker tag framefactory/node:20.0.0 framefactory/node:latest
