05cd33cb465de2045ea1cff7e1bd17d82aa43c86
Ralph Wiedemeier <ralph@framefactory.ch>
2023-09-30 16:58:36 +0200

    update node version to 20.8.0

--- images/node/build.sh
+++ images/node/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t framefactory/node:20.3.0 .
-docker tag framefactory/node:20.3.0 framefactory/node:latest
+docker build -t framefactory/node:20.8.0 .
+docker tag framefactory/node:20.8.0 framefactory/node:latest
--- images/node/run.sh
+++ images/node/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name node-bash --rm -ti framefactory/node:latest bash
+docker run --name node-bash --rm -ti framefactory/node:20.8.0 bash
